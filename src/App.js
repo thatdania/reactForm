b0e58214230ae4685cd7d64d6843857c3d022291
thatdania@gmail.com <thatdania@gmail.com>
2018-01-24 22:07:17 +0000

    Make a component file to export and insert in another file.
    I learnt that you can make components in other files and import it into different files just by calling a class. Given it uses ES6 syntax, I also have learnt that the current format of the function in Person.js benefits this which is another discovery to seek. However it basically works.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1> Hi I am a react App</h1>
-        <p> Is this working? </p>
+        <p> Is thiz working? </p>
+        <Person/>
       </div>
     );
   }
---
+++ src/Person/Person.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+const person = () => {
+  return <p> I am a Dania who is a person </p>
+};
+
+export default person;
