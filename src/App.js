d607e540d3207f7b01b83a344df74c3e6c06c082
thatdania@gmail.com <thatdania@gmail.com>
2018-01-25 09:42:19 +0000

    Learnt about state
    Instead of permenantly fixing the values, I can place it all in one place called state. Currently, state is an array of objects consisting of the name and age. The outcome is the same but the way we use this and state to call the objects in the array is formatted differently. If state is changed, React will re-render the Dom/update the DOm

--- src/App.js
+++ src/App.js
@@ -3,14 +3,21 @@ import './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
+  state = {
+    persons: [
+      { name:'Max', age:'28'},{ name:'Marco', age:'26'},{name:'Dania', age:'23'}
+,    ]
+  }
+
   render() {
     return (
       <div className="App">
         <h1> Hi I am a react App</h1>
         <p> Is thiz working? </p>
-        <Person name="Marco" age="26"/>
-        <Person name="Dania" age="23"> Hobby: Youtube </Person>
-        <Person name="Papi" age="60"/>
+        <button> Switch Name </button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}> Hobby: Youtube </Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
   }
